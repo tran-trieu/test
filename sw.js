a9e7125ffeef48d22e577a3a823f37d147d0140c
Tran Trieu <tran.trieu@web.de>
2018-07-31 17:31:37 +0200

    e

--- sw.js
+++ sw.js
@@ -5,24 +5,24 @@ self.addEventListener('install', function(event) {
 		caches.open(staticCacheName).then(function(cache) {
 			return cache.addAll([
 				'./',
-				'./index.html',
-				'./restaurant.html',
-				'./css/styles.css',
-				'./data/restaurants.json',
-				'./js/dbhelper.js',
-				'./js/main.js',
-				'./js/restaurant_info.js',
-				'./js/sw_register.js',
-				'./img/1.jpg',
-				'./img/2.jpg',
-				'./img/3.jpg',
-				'./img/4.jpg',
-				'./img/5.jpg',
-				'./img/6.jpg',
-				'./img/7.jpg',
-				'./img/8.jpg',
-				'./img/9.jpg',
-				'./img/10.jpg'
+				'index.html',
+				'restaurant.html',
+				'css/styles.css',
+				'data/restaurants.json',
+				'js/dbhelper.js',
+				'js/main.js',
+				'js/restaurant_info.js',
+				'sw_register.js',
+				'img/1.jpg',
+				'img/2.jpg',
+				'img/3.jpg',
+				'img/4.jpg',
+				'img/5.jpg',
+				'img/6.jpg',
+				'img/7.jpg',
+				'img/8.jpg',
+				'img/9.jpg',
+				'img/10.jpg'
 			]);
 		})
 	);
